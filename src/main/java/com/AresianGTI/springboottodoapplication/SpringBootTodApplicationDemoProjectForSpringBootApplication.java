04103a779ac43a046ecdbb655e234e9616e3d052
AresianGTI <onobenik@freenet.de>
2022-11-19 12:12:10 +0100

    initial commit

---
+++ src/main/java/com/AresianGTI/springboottodoapplication/SpringBootTodApplicationDemoProjectForSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.AresianGTI.springboottodoapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTodApplicationDemoProjectForSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTodApplicationDemoProjectForSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AresianGTI/springboottodoapplication/SpringBootTodApplicationDemoProjectForSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AresianGTI.springboottodoapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTodApplicationDemoProjectForSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
